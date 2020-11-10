ee4319dde74a2e4c416108d853f24ff28f16bc98
namcoi99 <nam_1999_x@yahoo.com.vn>
2020-11-10 11:30:57 +0700

    store info

---
+++ Frontend/src/containers/StoreInfo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StoreInfo extends Component {
+    render() {
+        return (
+            <div>
+                This is Store Info.
+            </div>
+        );
+    }
+}
+
+export default StoreInfo;
