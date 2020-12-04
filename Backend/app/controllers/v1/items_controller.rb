96dcad4343f8cfd2ccc65f166b0d16e5ecf178b8
chien <chiendoanngoc1705@gmail.com>
2020-12-05 00:06:22 +0700

    add item's API

--- Backend/app/controllers/v1/items_controller.rb
+++ Backend/app/controllers/v1/items_controller.rb
@@ -8,12 +8,47 @@ class V1::ItemsController < ApplicationController
 
   #GET /items/:id
   def show
-    @item = Item.where(id: params[:id]).first
+    @item = Item.find(params[:id])
     
     if @item != nil
-        render json: @item, status: :ok
+      render json: @item, status: :ok
     else
-        head(:unprocessable_entity)
+      head(:unprocessable_entity)
     end
+  end
+
+  #POST /items
+  def create
+    @item = Item.new(item_params)
+    @item.save
+
+    render json: @item, status: :created
+  end
+
+  #PUT /items/:id
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      render json: @item, status: :accepted
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  #DELETE /items/:id
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy      
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description)
   end
 end
