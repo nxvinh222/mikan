5a85324e18839b8818f53dad95d3d1459029e37d
chien <chiendoanngoc1705@gmail.com>
2020-12-08 00:21:02 +0700

    sign up

--- Backend/app/controllers/v1/users_controller.rb
+++ Backend/app/controllers/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json:{
+        id: @user.id,
+        username: @user.username,
+        shop_id: @user.shop_id,
+        admin: @user.admin
+      }, status: :created
+    else
+      render json: {
+        message: "Sign up fail"
+      }, status: :ok
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :shop_id, :admin)
+  end
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :items
     post  '/login',   to: 'sessions#create'
     delete  '/logout', to: 'sessions#destroy'
+
+    post '/signup', to: 'users#create'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
