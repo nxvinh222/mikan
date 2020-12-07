270bc912bed7ce8b54d6aa8d52ec6927a2387659
Vinh <nxvinh222@gmail.com>
2020-12-07 21:13:57 +0700

    Fix: item_quantities controller

--- Backend/app/controllers/v1/item_quantities_controller.rb
+++ Backend/app/controllers/v1/item_quantities_controller.rb
@@ -1,16 +1,15 @@
 class V1::ItemQuantitiesController < ApplicationController
     #POST /shops/items
     def addItems
-        @item_quantity = ItemQuantity.where(
-            shop_id: params[:shop_id],
-            item_id: params[:item_id]
-        )
-        if @item_quantity != nil
-            render json: {
-                createSucceed: false,
-                message: "Item already exist"
-            }
-        else
+        # @item_quantity = ItemQuantity.where(
+        #     shop_id: params[:shop_id]
+        # ).where(item_id: params[:item_id])
+        # if @item_quantity != nil
+        #     render json: {
+        #         createSucceed: false,
+        #         message: "Item already exist"
+        #     }
+        # else
             @item_quantity = ItemQuantity.new(item_quantity_params)
             @item_quantity.sold = 0
             @item_quantity.save
@@ -18,7 +17,7 @@ class V1::ItemQuantitiesController < ApplicationController
                 createSucceed: true,
                 message: "Add item succeed"
             }
-        end
+        # end
     end
     private
     def item_quantity_params
